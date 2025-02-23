35fb4e4617e203b5601b40de53fbdfe15e3e8401
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-23 17:32:21 +0530

    vercel.json file added

--- frontend/src/Components/Empty.jsx
+++ frontend/src/Components/Empty.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Empty() {
   return (
     <div className='w-[100vw] h-[100%] flex flex-col items-center justify-center mt-10'>
-        <img src="./src/assets/empty.png" className='w-[40%] md:w-[20%] mt-10' />
+        <img src="https://i.ibb.co/nMWRh0V8/empty.png" className='w-[40%] md:w-[20%] mt-10' />
         <h1 className='text-2xl font-semibold'>No such task available</h1>
     </div>
   )
